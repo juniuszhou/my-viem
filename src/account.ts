3e48ce21e06ad47aed3495be675215bfe0fe52d4
juniuszhou <junius.zhou@gmail.com>
2024-12-25 23:48:18 +0800

    account part

--- src/account.ts
+++ src/account.ts
@@ -1,13 +1,35 @@
-import { privateKeyToAccount } from 'viem/accounts' 
-
+import { privateKeyToAccount, mnemonicToAccount } from 'viem/accounts' 
+import { ethers } from "ethers";
 
 function get_account() {
-    const account = privateKeyToAccount('0x...');
+    require('dotenv').config();
+    const privateKey = process.env.PRIVATE_KEY;
+
+    if (!privateKey) {
+        throw new Error("PRIVATE_KEY is not defined in the environment variables.");
+    }
+
+    const account = privateKeyToAccount(`0x${privateKey}`);
+
+    const mnemonic = process.env.MNEMONIC;
+
+    if (!mnemonic) {
+        throw new Error("PRIVATE_KEY is not defined in the environment variables.");
+    }
+
+    const account2 = mnemonicToAccount(mnemonic);
+    console.log(account2.address);
+
     return account;
 }
 
+export function generateRandomAddress() {
+    const wallet = ethers.Wallet.createRandom();
+    return wallet;
+  }
+
+
 export {
-    
     get_account
-    
 }
+
